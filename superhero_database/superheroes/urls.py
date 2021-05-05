6bce48a0a2ecdd6c4caab403d90175d53e00e1e9
DanielPichette <daniel.f.pichette@gmail.com>
2021-05-05 13:54:37 -0500

    updated index.html. added an <a> link to the method/loop

--- superhero_database/superheroes/urls.py
+++ superhero_database/superheroes/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:hero_id>/', views.detail, name='detail')
+    path('<int:hero_id>/', views.detail, name='detail'),
 ]
