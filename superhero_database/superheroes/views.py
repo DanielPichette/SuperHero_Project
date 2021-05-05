d851cf9fef45e7a876edb7e709b933a12284846d
DanielPichette <daniel.f.pichette@gmail.com>
2021-05-05 14:39:18 -0500

    updated views.py. crated the create method

--- superhero_database/superheroes/views.py
+++ superhero_database/superheroes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Hero
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
@@ -16,3 +18,16 @@ def detail(request, hero_id):
         'hero': hero
     }
     return render(request, 'superheroes/detail.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        secret_identity = request.POST.get("secret identity")
+        main_power = request.POST.get("main power")
+        secondary_power = request.POST.get("secondary power")
+        catchphrase = request.POST.get("catchphrase")
+        new_hero = Hero(name=name, secret_identity=secret_identity, main_power=main_power, secondary_power=secondary_power, catchphrase=catchphrase)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
